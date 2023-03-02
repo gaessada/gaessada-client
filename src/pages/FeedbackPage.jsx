3d339baf56c48faa777ea4288178b19f3dacf31c
wnstjr <wnstjr8717@gmail.com>
2023-03-02 23:22:54 +0900

    feedback page default set

--- src/pages/FeedbackPage.jsx
+++ src/pages/FeedbackPage.jsx
@@ -1,14 +1,30 @@
-import {useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 import '../css/Handsontable.css'
 import SheetWindow from "../components/SheetWindow";
+import {v4} from "uuid";
 
 
-const FeedbackPage = (props) => {
+const FeedbackPage = () => {
+    const pageRef = useRef(null)
+    const [pageRefSize, setPageRefSize] = useState({width: 0, height: 0})
     const hotRef = useRef(null)
+    const hotDataRef = useRef(Array.from({length: 100}, () => ({rowId: v4()})))
 
-    return <>
-        <SheetWindow hotRef={hotRef}/>
-    </>
+    useEffect(() => {
+        const observer = new ResizeObserver((entries) => {
+            for (let entry of entries) {
+                const {width, height} = entry.contentRect;
+                setPageRefSize({...pageRefSize, width: width, height: height})
+            }
+        });
+        observer.observe(pageRef.current);
+        return () => observer.disconnect()
+    }, []);
+
+    return <div ref={pageRef} className={'w-full h-screen bg-gray-300'}>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10%'}}>SHEET</div>
+        <SheetWindow hotRef={hotRef} hotRefSize={pageRefSize} hotDataRef={hotDataRef}/>
+    </div>
 }
 
 export default FeedbackPage
